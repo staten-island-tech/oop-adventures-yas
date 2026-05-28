88b0d4149ed5fd112300e6e5549cc991809ff3a8
BathTubTheGreat <yilyanel@nycstudents.net>
2026-05-28 10:17:13 -0400

    dgmxmfmxf

--- game_interface.py
+++ game_interface.py
@@ -32,7 +32,7 @@ class Interface(arcade.View):
         self.tile_map = None
         self.camera_sprites = arcade.Camera2D()
         self.camera_gui = arcade.Camera2D()
-        self.player_hero = hero(money=500, hunger=100, health=100, strength=10, equipped_spell=None, level=1, charisma=5, xp_req=100, xp=0, stat_points=0, armor=5, weapon=10)
+        self.player_hero = hero(money=500, inventory=[], hunger=100, health=100, strength=10, equipped_spell=None, level=1, charisma=5, xp_req=100, xp=0, stat_points=0, armor=5, mana=100)
         self.player_light = None
     def setup(self):
 
